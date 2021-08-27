fad7e5a4d33c0c3539cafa279ed39b0b69daa862
Shivam Garg <shivamgarg@Shivams-MacBook-Air.local>
2021-08-26 22:47:20 -0400

    final changes

--- backend/index.js
+++ backend/index.js
@@ -6,5 +6,3 @@ const app = express();
 mongoose.connect('mongodb+srv://ratemyfoodadmin:YNvYjNoPe3KS3p7i@ratemyfood.jud4q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true}).then(()=>{app.listen(3000, "Server is live")});
 
 
-
-app.listen(3000, "Server is live")
---
+++ backend/models/review.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+    comment: {
+        required: true,
+        type: 
+    }
+    rating: {
+        required: true,
+    }
+})
