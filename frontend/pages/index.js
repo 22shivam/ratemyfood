dfba832f557359265fcb6a665c6da9cb3fcc175a
Shivam Garg <shivamg2604@gmail.com>
2021-08-27 16:19:48 -0400

    he

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -38,7 +38,7 @@ export default function Home() {
       
       <center><div className=" w-4/12 flex items-center p-6 space-x-6 h-20 rounded-3xl shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-500 opacity-70" style={{backgroundColor: "#723D46"}}>
       <div className="flex bg-gray-100 p-4 w-72 space-x-4 rounded-lg" style={{backgroundColor: "#723D46"}}>
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-6 opacity-30" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-6 opacity-30" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
         <input className="bg-gray-100 outline-none" type="text" placeholder="search for school" style={{backgroundColor: "#723D46"}}/>
