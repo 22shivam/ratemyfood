aba4a8a9a7e1a28ab86db16d74f08d31f77c1ca9
Shivam Garg <shivamg2604@gmail.com>
2021-08-27 17:22:52 -0400

    final changes

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import mainPic from '../public/homepagepic.png'
+import { useState } from 'react'
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState();
   return (
     <div className="container sm mx-auto">
       <Head>
@@ -16,8 +20,8 @@ export default function Home() {
         <link rel="shortcut icon" href="/favicon_package_v0/favicon.ico" />
         <meta name="theme-color" content="#ffffff"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/> 
-<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-<link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet"></link>
       </Head>
 
       <br/>
@@ -28,26 +32,35 @@ export default function Home() {
       <br/>
 
       <center><div>
+        {/* dimensions are 1200*630 */}
+        <Image src={mainPic} className="md:bg-contain" width={600} height={315} />
+        <br/>
+      <br/>
         <p style={{fontFamily: "comfortaa", fontSize: "30px"}}>
           ratemyprofessor, but for food
         </p>
       </div></center>
       <br/>
       <br/>
-
       
+
+      <form method="POST" action="/api/search">
       <center><div className=" w-4/12 flex items-center p-6 space-x-6 h-20 rounded-3xl shadow-lg hover:shadow-xl transform hover:scale-105 transition duration-500 opacity-70" style={{backgroundColor: "#723D46"}}>
       <div className="flex bg-gray-100 p-4 w-72 space-x-4 rounded-lg" style={{backgroundColor: "#723D46"}}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-6 opacity-30" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
-        <input className="bg-gray-100 outline-none" type="text" placeholder="search for school" style={{backgroundColor: "#723D46"}}/>
+        <input className="bg-gray-100 outline-none" type="text" value={searchValue} onChange={(e)=>{setSearchValue(e.target.value)}} placeholder="search for school" style={{backgroundColor: "#723D46"}}/>
       </div>
-      <div className="bg-red-600 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer" style={{backgroundColor: "#923D46"}}>
-        <span>Search</span>
+      <div>
+        <input type="Submit" value="Submit" className="bg-red-600 py-3 px-5 text-white font-semibold rounded-lg hover:shadow-lg transition duration-3000 cursor-pointer" style={{backgroundColor: "#923D46"}}>
+          </input>
       </div>
-    </div>
+      </div>
     </center>
+    </form>
+    
+   
 </div>
   )
 }
