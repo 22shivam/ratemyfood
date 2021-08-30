4a1892512d676f7fc8f15a6d69d74868df72db4e
Shivam Garg <shivamg2604@gmail.com>
2021-08-30 19:53:30 -0400

    added analytics

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -8,18 +8,18 @@ function MyApp({ Component, pageProps }) {
   return (<>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charset = "UTF-8" />
+      <meta charset="UTF-8" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-C3ZGHKCJ2Z"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap" rel="stylesheet"></link>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-C3ZGHKCJ2Z"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag("js", new Date());
 
-  gtag('config', 'G-C3ZGHKCJ2Z');
-</script>
+        gtag("config", "G-C3ZGHKCJ2Z");
+      </script>
     </Head>
     <Component {...pageProps} />
   </>)
